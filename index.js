764560b9268a0fe8dbefe595d7bd38ece7a2f294
Josh <josh@Joshs-Laptop.local>
2024-10-22 18:25:57 +0530

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const writeReview = require("./review");
 const cron = require("node-cron");
 
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("0 30 0 * * *", async () => {
   console.log("Running Automation");
   await writeReview();
   console.log("completed Automation");
